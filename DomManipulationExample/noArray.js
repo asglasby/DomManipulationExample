37115ec918f49e17c3e3b4f9d4cc18dcef52302a
asglasby <aishaextra@gmail.com>
2014-05-07 23:59:37 -0400

    added strike through and hide using css

--- DomManipulationExample/noArray.js
+++ DomManipulationExample/noArray.js
@@ -1,13 +1,28 @@
-﻿var addToDoItem = function () {
+﻿var myListItemCounter = 0;
+
+var addToDoItem = function () {
     "use strict"
-    var myListItemCounter = 0;
     if (document.getElementById("input").value === "") {  //if the user does not enter any text or item, don't let them move on
 
         alert("Please Enter An Item to add to the list");
     } else {
+        
         myListItemCounter++;
         var myNewListItem = document.getElementById("input").value;
-        document.getElementById("orderedlist").innerHTML += '<li id="listItem' + myListItemCounter + '"> "'+  myNewListItem +    ' ---Click to Delete--- '+ myListItemCounter + '"</li>';
+        document.getElementById("orderedlist").innerHTML += '<li id="hideItem' + myListItemCounter + '"><span id="listItem' + myListItemCounter + '" onclick="strikeOutListItem(\'listItem' + myListItemCounter + '\');"> "' + myNewListItem + + myListItemCounter + ' "</span>' + '<span id="listItem' + myListItemCounter + '" onclick="hideListItem(\'hideItem' + myListItemCounter + '\');">  ---Click to Delete--- </span></li>';
+
         document.getElementById("input").value = "";
     }
 };
+
+var strikeOutListItem = function (listItemId) {
+    "use strict";
+    document.getElementById(listItemId).setAttribute("class", "strikethrough");
+};
+
+var hideListItem = function (hideItemId) {
+    "use strict";
+    document.getElementById(hideItemId).setAttribute("class", "hideItem");
+};
+
+//document.getElementById("orderedlist").innerHTML = " ";
