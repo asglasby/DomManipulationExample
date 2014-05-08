a23d3e58f0eef37f96eca0897090bf5d82cc0eaa
asglasby <aishaextra@gmail.com>
2014-05-07 21:29:51 -0400

    added counter

--- DomManipulationExample/noArray.js
+++ DomManipulationExample/noArray.js
@@ -1,12 +1,13 @@
 ﻿var addToDoItem = function () {
     "use strict"
-    var myToDoListArray = [];
+    var myListItemCounter = 0;
     if (document.getElementById("input").value === "") {  //if the user does not enter any text or item, don't let them move on
 
         alert("Please Enter An Item to add to the list");
     } else {
+        myListItemCounter++;
         var myNewListItem = document.getElementById("input").value;
-            document.getElementById("orderedlist").innerHTML += "<li>" + myNewListItem + "</li>";       
-            document.getElementById("input").value = "";
+        document.getElementById("orderedlist").innerHTML += '<li id="listItem' + myListItemCounter + '"> "'+  myNewListItem +    ' ---Click to Delete--- '+ myListItemCounter + '"</li>';
+        document.getElementById("input").value = "";
     }
 };
